7adc1ffafd203e63131368008e96fa8200c6ce6e
Shiyan Boxer <shiyanboxer@gmail.com>
2024-01-08 12:39:16 -0500

    Update customSort.ts

--- next-typescript/src/app/utils/customSort.ts
+++ next-typescript/src/app/utils/customSort.ts
@@ -1,14 +1,14 @@
 export function customSort(input: string, customAlphabet: string): string {
-    if (new Set(customAlphabet).size !== 26) {
-        throw new Error("Custom alphabet must contain all 26 letters");
-    }
+  if (new Set(customAlphabet).size !== 26) {
+      throw new Error("Custom alphabet must contain all 26 letters");
+  }
 
-    const charPositions = new Map<string, number>();
-    for (let i = 0; i < customAlphabet.length; i++) {
-        charPositions.set(customAlphabet[i], i);
-    }
+  const charPositions = new Map<string, number>();
+  customAlphabet.toLowerCase().split('').forEach((char, index) => {
+      charPositions.set(char, index);
+  });
 
-    return input.split('').sort((a, b) => {
-        return (charPositions.get(a.toLowerCase()) ?? 0) - (charPositions.get(b.toLowerCase()) ?? 0);
-    }).join('').toLowerCase();
+  return input.toLowerCase().split('').sort((a, b) => {
+      return (charPositions.get(a) ?? 0) - (charPositions.get(b) ?? 0);
+  }).join('');
 }
